8d36789df3e0ea5ecb7f8351e813f3e811979913
curiousidy <idairaalemanquintana@gmail.com>
2022-02-27 15:16:19 +0000

    primera copia bootcamp

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by idaira aleman quintana on 12/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
